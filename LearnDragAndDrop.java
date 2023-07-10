61d7a499e46565013577535f0cecbda3c16f886a
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2023-07-10 08:32:35 +0530

    LearnDragAndDrop

---
+++ LearnDragAndDrop.java
@@ -0,0 +1,36 @@
+package week3;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class LearnDragAndDrop {
+	
+	@Test
+	public void DragandDrop()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.leafground.com");
+		driver.findElement(By.xpath("//form[@id='menuform']/ul/li[2]")).click();
+		driver.findElement(By.xpath("//span[text()='Drag']")).click();
+		Point postion= driver.findElement(By.xpath("//div[@id='form:conpnl']")).getLocation();
+		System.out.println(postion);
+		Actions builder= new Actions(driver);
+		
+		builder.dragAndDropBy(driver.findElement(By.xpath("//div[@id='form:conpnl']")),150,0).perform();
+		Point postion1= driver.findElement(By.xpath("//div[@id='form:conpnl']")).getLocation();
+		System.out.println(postion1);
+	}
+}
