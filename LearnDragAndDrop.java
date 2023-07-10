f15193b621293973e0935d2bf17cb604d5ddab94
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2023-07-10 09:54:42 +0530

    updated dragan drop

--- LearnDragAndDrop.java
+++ LearnDragAndDrop.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -32,5 +33,17 @@ public class LearnDragAndDrop {
 		builder.dragAndDropBy(driver.findElement(By.xpath("//div[@id='form:conpnl']")),150,0).perform();
 		Point postion1= driver.findElement(By.xpath("//div[@id='form:conpnl']")).getLocation();
 		System.out.println(postion1);
+		
+		//drop to the Target
+		System.out.println(driver.findElement(By.xpath("//p[@class='ui-widget-header']")).getText());
+		
+		WebElement drop=driver.findElement(By.xpath("//div[@id='form:drag_content']"));
+		builder = new Actions(driver);
+		WebElement target= driver.findElement(By.xpath("//div[@id='form:drop']"));
+		builder.dragAndDrop(drop, target).perform();
+		System.out.println(driver.findElement(By.xpath("//p[@class='ui-widget-header']")).getText());
+		
+		
+		driver.close();
 	}
 }
