34935211644a4c9e460fe6b07046521fd420673d
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2023-07-14 22:49:44 +0530

    updated LearnTextBox

--- LearnTextBox.java
+++ LearnTextBox.java
@@ -6,13 +6,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 public class LearnTextBox {
 	@Test
-	public void textBox() {
+	public void textBox() throws InterruptedException {
 	WebDriverManager.chromedriver().setup();
 	WebDriver driver = new ChromeDriver();
 	driver.manage().window().maximize();
@@ -24,6 +25,22 @@ public class LearnTextBox {
 	driver.findElement(By.xpath("//h5[contains(text(),'Append Country')]/following::input")).sendKeys(" India");
 	boolean bool=driver.findElement(By.xpath("//h5[contains(text(),'Verify if text')]/following::input")).isEnabled();
 	System.out.println(bool);
+	driver.findElement(By.xpath("//h5[contains(text(),'Clear the typed')]/following::input")).clear();
+	
+	System.out.println(driver.findElement(By.xpath("//h5[contains(text(),'Retrieve')]/following::input")).getAttribute("value"));
+	
+	driver.findElement(By.xpath("//h5[contains(text(),'Type email')]/following::input")).sendKeys("xyz@gmail.com"+Keys.TAB);
+	driver.findElement(By.xpath("//h5[contains(text(),'Just Press')]/following::input[2]")).sendKeys(Keys.ENTER);
+	
+	WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+	wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[contains(text(),'Age is mandatory')]"))));
+	System.out.println("Error Message displayed");
+	
+	Thread.sleep(5000);
+	
+	
+	driver.close();
+	
 	
 	}
 }
