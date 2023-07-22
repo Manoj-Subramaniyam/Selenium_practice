8bb3868dcbaaa27597e3d61e57c3f0c1daff99c8
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2023-07-22 23:40:21 +0530

    updated Learn CheckBox

--- LearnCheckBox.java
+++ LearnCheckBox.java
@@ -4,6 +4,7 @@ package week4;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -16,7 +17,7 @@ import org.testng.annotations.Test;
 public class LearnCheckBox {
 	
 	@Test
-	public void checkBox()
+	public void checkBox() throws InterruptedException
 	{
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
@@ -30,6 +31,21 @@ public class LearnCheckBox {
 		driver.findElement(By.xpath("//span[text()='Basic']/preceding-sibling::div[1]")).click();
 		if(ele.isSelected())
 			System.out.println("clicked");
+		driver.findElement(By.xpath("//span[text()='Ajax']/preceding-sibling::div[1]")).click();
+		ele= driver.findElement(By.xpath("//div[@role='alert']/div/span"));
+		
+		if(ele.isDisplayed()) {
+			System.out.println("pop up displayed");
+			System.out.println(ele.getText());
+			driver.findElement(By.xpath("//span[text()='Ajax']/preceding-sibling::div[1]")).click();
+			//Thread.sleep(5000);
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Unchecked']")));
+			ele= driver.findElement(By.xpath("//span[text()='Unchecked']"));
+			wait.until(ExpectedConditions.refreshed(ExpectedConditions.stalenessOf(ele)));
+			System.out.println(ele.getText());
+			}
+		
+		driver.close();
 	}
 
 }
