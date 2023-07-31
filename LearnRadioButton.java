b4026fbefc57f4bcaa4ea90fcf4f09b06f6f0834
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2023-07-31 14:18:18 +0530

    updated Learn RadioButton

--- LearnRadioButton.java
+++ LearnRadioButton.java
@@ -1,11 +1,14 @@
 package week4;
 
+import java.util.ArrayList;
+
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 public class LearnRadioButton {
 		@Test
 		public void radioButton() throws InterruptedException
@@ -20,7 +23,25 @@ public class LearnRadioButton {
 			
 			if(driver.findElement(By.xpath("//label[text()='Chrome']/preceding-sibling::div/div/input")).isSelected())
 				System.out.println("Chrome is selected");
+			driver.findElement(By.xpath("//label[text()='Bengaluru']")).click();
 			
+			if(driver.findElement(By.xpath("//label[text()='Bengaluru']/preceding-sibling::div/div/input")).isSelected())
+				System.out.println("Bengaluru is selected");
+			driver.findElement(By.xpath("//label[text()='Bengaluru']")).click();
+			if(!driver.findElement(By.xpath("//label[text()='Bengaluru']/preceding-sibling::div/div/input")).isSelected())
+				System.out.println("Bengaluru is Unselected");
+			
+			ArrayList<WebElement> lstOfWebElements= new ArrayList<WebElement>(driver.findElements(By.xpath("//h5[contains(text(),'Find')]/following-sibling::div//div/input")));
+			int i=0;
+			for (WebElement temp: lstOfWebElements)
+			{
+				i+=1;
+				if(temp.isSelected()) {
+					
+					System.out.println(driver.findElement(By.xpath("(//h5[contains(text(),'Find')]/following-sibling::div//div/input)["+i+"]/following::label")).getText());
+				}
+					
+			}
 			Thread.sleep(3000);
 			driver.close();
 ;			
