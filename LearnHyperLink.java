bdf7c4a2adf53b20d82ff931414b1cc93a7df691
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2023-08-01 20:40:17 +0530

    Updated Learn HyperLink

--- LearnHyperLink.java
+++ LearnHyperLink.java
@@ -3,6 +3,9 @@ package week5;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,7 +23,14 @@ public class LearnHyperLink {
 		gotoHyperLink();
 		driver.findElement(By.linkText("Go to Dashboard")).click();
 		gotoHyperLink();
-		
+		List<WebElement> lstoflinks=driver.findElements(By.xpath("//div[@class='layout-main-content']//a"));
+		for(int i=1;i<lstoflinks.size();i++)
+		{
+			if(lstoflinks.get(i).getAttribute("href").equals(driver.findElement(By.linkText("Go to Dashboard")).getAttribute("href")))
+				System.out.println("Duplicated link is : "+lstoflinks.get(i).getAttribute("href"));
+		}
+		String link=driver.findElement(By.linkText("Find the URL without clicking me.")).getAttribute("href");
+		System.out.println(link);
 		Thread.sleep(3000);
 		driver.close();
 	}
