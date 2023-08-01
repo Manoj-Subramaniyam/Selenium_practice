ea82c3595ee62c1c110d52f887dc08153d6240f0
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2023-08-01 09:15:37 +0530

    updated Learn radiobutton

--- LearnRadioButton.java
+++ LearnRadioButton.java
@@ -36,14 +36,31 @@ public class LearnRadioButton {
 			for (WebElement temp: lstOfWebElements)
 			{
 				i+=1;
-				if(temp.isSelected()) {
-					
+				if(temp.isSelected()) 
 					System.out.println(driver.findElement(By.xpath("(//h5[contains(text(),'Find')]/following-sibling::div//div/input)["+i+"]/following::label")).getText());
+			}
+			lstOfWebElements=new ArrayList<WebElement>(driver.findElements(By.xpath("(//div[@class='ui-g'])[2]//div[@class='ui-helper-hidden-accessible']/input")));
+			int j,age=19;
+			String[] age1,age3;
+			String age2;
+			boolean flag=false;
+			for(int l=0;l< lstOfWebElements.size();l++)
+			{
+				age1=lstOfWebElements.get(l).getAttribute("value").split(" ");
+				age3=age1[0].split("-");
+				for(int k=0;k<age3.length;k++)
+				{
+					j=Integer.parseInt(age3[k]);
+					if(j>=age)
+					{
+						if(!lstOfWebElements.get(l).isSelected()) driver.findElement(By.xpath("((//div[@class='ui-g'])[2]//div[@class='ui-helper-hidden-accessible']/input)["+(l+1)+"]/following::div")).click();
+						flag=true;
+						break;
+					}
 				}
-					
+				if(flag) break;
 			}
 			Thread.sleep(3000);
 			driver.close();
-;			
 		}
 }
